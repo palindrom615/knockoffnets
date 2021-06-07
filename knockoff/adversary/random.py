b827e157746dfb744770db51629d026b6bd97067
Alexander Schloegl <alexnader.schloegl@uibk.ac.at>
2021-06-07 10:28:45 +0200

    Move RandomAdevrsary to own file, clean imports

--- knockoff/adversary/random.py
+++ knockoff/adversary/random.py
@@ -2,13 +2,15 @@
 """This is a short description.
 Replace this with a more detailed description of what this file contains.
 """
+import argparse
+import os.path as osp
+import os
 
 import numpy as np
 
 from tqdm import tqdm
 
 import torch
-
 import knockoff.config as cfg
 
 __author__ = "Tribhuvanesh Orekondy"
@@ -16,17 +18,13 @@ __maintainer__ = "Tribhuvanesh Orekondy"
 __email__ = "orekondy@mpi-inf.mpg.de"
 __status__ = "Development"
 
-
-class AdaptiveAdversary(object):
-    def __init__(self, blackbox, queryset, batch_size=8, num_workers=15, flush_interval=1000):
+class RandomAdversary(object):
+    def __init__(self, blackbox, queryset, batch_size=8):
         self.blackbox = blackbox
         self.queryset = queryset
-        self.flush_interval = flush_interval
 
         self.n_queryset = len(self.queryset)
         self.batch_size = batch_size
-        self.num_workers = num_workers
-        self.queryloader = None
         self.idx_set = set()
 
         self.transferset = []  # List of tuples [(img_path, output_probs)]
@@ -50,12 +48,27 @@ class AdaptiveAdversary(object):
                                         size=min(self.batch_size, budget - len(self.transferset)))
                 self.idx_set = self.idx_set - set(idxs)
 
-                img_t = [self.queryset.samples[i][0] for i in idxs]  # Image paths
+                if len(self.idx_set) == 0:
+                    print('=> Query set exhausted. Now repeating input examples.')
+                    self.idx_set = set(range(len(self.queryset)))
+
                 x_t = torch.stack([self.queryset[i][0] for i in idxs]).to(self.blackbox.device)
                 y_t = self.blackbox(x_t).cpu()
 
+                if hasattr(self.queryset, 'samples'):
+                    # Any DatasetFolder (or subclass) has this attribute
+                    # Saving image paths are space-efficient
+                    img_t = [self.queryset.samples[i][0] for i in idxs]  # Image paths
+                else:
+                    # Otherwise, store the image itself
+                    # But, we need to store the non-transformed version
+                    img_t = [self.queryset.data[i] for i in idxs]
+                    if isinstance(self.queryset.data[0], torch.Tensor):
+                        img_t = [x.numpy() for x in img_t]
+
                 for i in range(x_t.size(0)):
-                    self.transferset.append((img_t[i], y_t[i].cpu().squeeze()))
+                    img_t_i = img_t[i].squeeze() if isinstance(img_t[i], np.ndarray) else img_t[i]
+                    self.transferset.append((img_t_i, y_t[i].cpu().squeeze()))
 
                 pbar.update(x_t.size(0))
 
--- knockoff/adversary/transfer.py
+++ knockoff/adversary/transfer.py
@@ -4,26 +4,17 @@ Replace this with a more detailed description of what this file contains.
 """
 import argparse
 from knockoff.adversary.adaptive import AdaptiveAdversary
+from knockoff.adversary.random import RandomAdversary
 import os.path as osp
 import os
 import pickle
 import json
 from datetime import datetime
 
-import numpy as np
-
-from tqdm import tqdm
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-import torchvision
 
 from knockoff import datasets
-import knockoff.utils.transforms as transform_utils
-import knockoff.utils.model as model_utils
 import knockoff.utils.utils as knockoff_utils
 from knockoff.victim.blackbox import Blackbox
 import knockoff.config as cfg
@@ -33,62 +24,6 @@ __maintainer__ = "Tribhuvanesh Orekondy"
 __email__ = "orekondy@mpi-inf.mpg.de"
 __status__ = "Development"
 
-
-class RandomAdversary(object):
-    def __init__(self, blackbox, queryset, batch_size=8):
-        self.blackbox = blackbox
-        self.queryset = queryset
-
-        self.n_queryset = len(self.queryset)
-        self.batch_size = batch_size
-        self.idx_set = set()
-
-        self.transferset = []  # List of tuples [(img_path, output_probs)]
-
-        self._restart()
-
-    def _restart(self):
-        np.random.seed(cfg.DEFAULT_SEED)
-        torch.manual_seed(cfg.DEFAULT_SEED)
-        torch.cuda.manual_seed(cfg.DEFAULT_SEED)
-
-        self.idx_set = set(range(len(self.queryset)))
-        self.transferset = []
-
-    def get_transferset(self, budget):
-        start_B = 0
-        end_B = budget
-        with tqdm(total=budget) as pbar:
-            for t, B in enumerate(range(start_B, end_B, self.batch_size)):
-                idxs = np.random.choice(list(self.idx_set), replace=False,
-                                        size=min(self.batch_size, budget - len(self.transferset)))
-                self.idx_set = self.idx_set - set(idxs)
-
-                if len(self.idx_set) == 0:
-                    print('=> Query set exhausted. Now repeating input examples.')
-                    self.idx_set = set(range(len(self.queryset)))
-
-                x_t = torch.stack([self.queryset[i][0] for i in idxs]).to(self.blackbox.device)
-                y_t = self.blackbox(x_t).cpu()
-
-                if hasattr(self.queryset, 'samples'):
-                    # Any DatasetFolder (or subclass) has this attribute
-                    # Saving image paths are space-efficient
-                    img_t = [self.queryset.samples[i][0] for i in idxs]  # Image paths
-                else:
-                    # Otherwise, store the image itself
-                    # But, we need to store the non-transformed version
-                    img_t = [self.queryset.data[i] for i in idxs]
-                    if isinstance(self.queryset.data[0], torch.Tensor):
-                        img_t = [x.numpy() for x in img_t]
-
-                for i in range(x_t.size(0)):
-                    img_t_i = img_t[i].squeeze() if isinstance(img_t[i], np.ndarray) else img_t[i]
-                    self.transferset.append((img_t_i, y_t[i].cpu().squeeze()))
-
-                pbar.update(x_t.size(0))
-
-        return self.transferset
 
 
 def main():
